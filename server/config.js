7b3aaddfe8f8bd43740cb2080745016aeb27f1d4
Wisdom <jslord21@gmail.com>
2017-12-16 23:58:57 +0100

    hooked up twitter

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: "FrcqhaRjIWuOOK81MJJ1o0Z4o",
+	consumer_secret: "Di1UrDTCy3PJhsaA8yVnyaUPEiRYuKXxtZHytSe8aP1tKYj8wB",
+	access_token_key: "882463969033826304-Wq2YBUHVYn2Ff5AtOdYY3MPTZvOYJCz",
+	access_token_secret: "ILiAjsVeKxoSoEOXNnkBtb6vn96XVB8hDHhtSj4B2AWlY"
+}
